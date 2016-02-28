03161bc5675b68d4c6c5afed71f4662455cf54f6
dhamilton91 <dhamilton91@hotmail.com>
2016-02-28 16:58:14 +0000

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     context: __dirname,
